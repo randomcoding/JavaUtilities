b964ec421613f571f0090f11952af6e1b9a61041
Tym the Enchanter <tymtheenchanter@randomcoding.co.uk>
2010-06-05 16:26:14 +0100

    Added tests project, libraries and config.

---
+++ RandomCodingJavaUtilitiesTests/tests/uk/co/randomcoding/java/util/collection/TypedCollectionUtilitiesTest.java
@@ -0,0 +1,62 @@
+/*
+ * (c) Tym The Enchanter (tymtheenchanter@randomcoding.co.uk), 5 Jun 2010
+ */
+package uk.co.randomcoding.java.util.collection;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.hamcrest.Matchers;
+import org.junit.Test;
+
+/**
+ * Created on: 5 Jun 2010<br>
+ * 
+ * @author Tym the Enchanter <tymtheenchanter@randomcoding.co.uk>
+ */
+public class TypedCollectionUtilitiesTest
+{
+	@Test
+	public void testExtractTypedCollection() throws Exception
+	{
+		Collection<TestClass> testCollection = new HashSet<TypedCollectionUtilitiesTest.TestClass>();
+		testCollection.add(new TestClass());
+		testCollection.add(new TestClass());
+		testCollection.add(new TestSubClass());
+		testCollection.add(new TestSubClass());
+		testCollection.add(new TestSubSubClass());
+		testCollection.add(new TestSubSubClass());
+
+		assertThat(TypedCollectionUtilities.extractTypedCollection(testCollection, TestSubClass.class), hasSize(4));
+		assertThat(TypedCollectionUtilities.extractTypedCollection(testCollection, TestSubSubClass.class), hasSize(2));
+		assertThat(TypedCollectionUtilities.extractTypedCollection(testCollection, OtherTestClass.class), Matchers.<OtherTestClass> empty());
+	}
+
+	private static class TestClass
+	{
+		// empty class
+	}
+
+	private static class TestSubClass extends TestClass
+	{
+		// empty class
+	}
+
+	private static class TestSubSubClass extends TestSubClass
+	{
+		// empty class
+	}
+
+	private static class OtherTestClass
+	{
+		// empty class
+	}
+
+	private static class OtherTestSubClass extends OtherTestClass
+	{
+		// empty class
+	}
+}
